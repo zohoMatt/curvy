6539b5a0cde9ad473a7fa182af9de39446d8cf00
zohoMatt <mattzuo12@gmail.com>
2020-08-26 16:04:07 -0400

    feat: Run yarn build.

---
+++ docs/precache-manifest.db6e5f4699393e27fb9a14a3265a0d02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e40000caf97467182a",
+    "url": "/css/app.4a887012.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "178be45506be1adcbf57a0a348655133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd028424d6ac6e08d29d",
+    "url": "/js/about.b812659f.js"
+  },
+  {
+    "revision": "69e40000caf97467182a",
+    "url": "/js/app.1cf1a643.js"
+  },
+  {
+    "revision": "6468c4f7ced6b564b4a8",
+    "url": "/js/chunk-vendors.0a24a9c0.js"
+  },
+  {
+    "revision": "9234f1b34ddf08f6d69ad90e7e331fdd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
