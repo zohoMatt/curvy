fe28876ea5d56d986da664bf79e8a45beacfc7fa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-01 21:33:24 +0000

    Deploy zohoMatt/curvy to github.com/zohoMatt/curvy.git:gh-pages

---
+++ precache-manifest.c19b6a9fc44ef8909b1aafe535bff961.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e731e2ebad8f0232d7",
+    "url": "/curvy/css/app.f401c494.css"
+  },
+  {
+    "revision": "b3a8c88fe565304ef2ea",
+    "url": "/curvy/css/chunk-vendors.a03fbbcc.css"
+  },
+  {
+    "revision": "52e6b3fad1db931f0a280466a36e9258",
+    "url": "/curvy/index.html"
+  },
+  {
+    "revision": "b6e731e2ebad8f0232d7",
+    "url": "/curvy/js/app.f05442dd.js"
+  },
+  {
+    "revision": "b3a8c88fe565304ef2ea",
+    "url": "/curvy/js/chunk-vendors.ed1546fb.js"
+  },
+  {
+    "revision": "d7d65337b342e8754538",
+    "url": "/curvy/js/doc.50ec5241.js"
+  },
+  {
+    "revision": "9234f1b34ddf08f6d69ad90e7e331fdd",
+    "url": "/curvy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/curvy/robots.txt"
+  }
+]);
